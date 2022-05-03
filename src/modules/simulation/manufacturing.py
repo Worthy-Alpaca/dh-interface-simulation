90b93b362cce1f71ea1473790700fadbfb17caec
Worthy Alpaca <stephanxxschumacher>
2022-05-03 14:57:22 +0200

    added machine integration

--- src/modules/simulation/manufacturing.py
+++ src/modules/simulation/manufacturing.py
@@ -1,11 +1,16 @@
 import math
 import random
+import sys
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import deque
 from pathlib import Path
-from modules.simulation.cartsetup import CartSetup
-from modules.simulation.dataloader import DataLoader
+from multiprocessing import Pool
+PACKAGE_PARENT = '../'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+from simulation.machine import Machine
 global XMOD 
 XMOD = 1
 RANDOM_PROBLEMS = (0,0)#(30, 300)
@@ -22,24 +27,19 @@ class Manufacturing():
   - `machine`: either M10 (default) or M20
   
   """
-  def __init__(self, data: tuple, machine: str = 'M10') -> object:
+  def __init__(self, data: tuple, machine: Machine) -> object:
     # assign machine property
     self.machine = machine
     self.components = data[1]
     self.data = data[0]
     self.offsets = data[2]
 
-  def calcVector(self, vectorA: tuple, vectorB: tuple, velocity: float):
+  def __calcVector(self, vectorA: tuple, vectorB: tuple, velocity: float):
     """
     Calculates the pathlength between two given vectors
     - vectorA: where you are
     - vectorB: where you want to go 
     """
-    #print('Type: ', type(vectorA[0]), type(vectorA[1]), type(vectorB[0]), type(vectorB[1]))
-    #print('Vector teile: ',(vectorB[0]), (vectorA[0]), (vectorB[1]), (vectorA[1]))
-    #print()
-    #print((float(vectorB[0]) - float(vectorA[0])), (float(vectorB[1]) - float(vectorA[1])))
-    #print('===')
     vector_AB = ((float(vectorB[0]) - float(vectorA[0])), (float(vectorB[1]) - float(vectorA[1])))
     path_length = math.sqrt(vector_AB[0]**2 + vector_AB[1]**2)
     #print('path length:', path_length)
@@ -50,10 +50,6 @@ class Manufacturing():
 
     def isNan(string):
       return string != string
-
-    # the estimated velocity of the machine arm
-    # M20 = 1345.87 mm/s | M10 = 1621.4 mm/s
-    machine = self.machine
     
     #print(self.components)
     TIME = 0
@@ -73,10 +69,7 @@ class Manufacturing():
         #print(lookupTable)
         location_vector_A = (lookupTable.Pickup_X.max(), lookupTable.Pickup_Y.max())
         location_vector_B = ((row.X + OFFSET_X + offset_row.X), (row.Y + OFFSET_Y + offset_row.Y))
-        #print(lookupTable)
-        velocity = 1345.87 if str(machine) == "M20" else 1621.4
-        velocity = velocity * ( lookupTable.mean_acceleration.max() / 1000)
-        #velocity = velocity.max()
+        velocity = self.machine.velocity * ( lookupTable.mean_acceleration.max() / 1000)
         
         if multiPickOption == True:
           # picking components with multiple heads at once
@@ -94,14 +87,14 @@ class Manufacturing():
               next_index = next_index +1
             nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code'])]
             next_pickup = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
-            TIME = self.calcVector(location_vector_A, next_pickup, velocity) + TIME + PICKUP
+            TIME = self.__calcVector(location_vector_A, next_pickup, velocity) + TIME + PICKUP
 
           elif row.Task == 'End Multiple Pickup':
             # calculate the path to the current component
             loc_vector_A = (lookupTable.Pickup_X.max(), lookupTable.Pickup_Y.max())
             loc_vector_B = ((row.X + OFFSET_X + offset_row.X), (row.Y + OFFSET_Y + offset_row.Y))
-            checkpoint = self.calcVector(loc_vector_A, CHECKPOINT, velocity)
-            path = self.calcVector(CHECKPOINT, loc_vector_B, velocity)
+            checkpoint = self.__calcVector(loc_vector_A, CHECKPOINT, velocity)
+            path = self.__calcVector(CHECKPOINT, loc_vector_B, velocity)
             TIME = path + TIME + DROPOFF + checkpoint
 
             # set the current component vector as the current postition
@@ -113,7 +106,7 @@ class Manufacturing():
             # loop over queue
             for i in multiPick:
               # calculate path and time between components
-              multiPath = self.calcVector(current_pos, i, velocity)
+              multiPath = self.__calcVector(current_pos, i, velocity)
               TIME = (multiPath) + TIME + DROPOFF
               current_pos = i
             multiPick.clear()
@@ -127,17 +120,17 @@ class Manufacturing():
               next_index = next_index +1
             nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )]
             next_pickup_vector = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
-            TIME = (self.calcVector(next_pickup_vector, current_pos, velocity)) + TIME + DROPOFF
+            TIME = (self.__calcVector(next_pickup_vector, current_pos, velocity)) + TIME + DROPOFF
 
           elif row.Task == 'Fiducial':
-            path_length = self.calcVector((0, 0), location_vector_B, velocity)
+            path_length = self.__calcVector((0, 0), location_vector_B, velocity)
             TIME = (path_length) + TIME 
 
           else:
             #print('==  single pick, multipick opt  ==')
             # calculate the path/time for a single pickup
-            path_length = self.calcVector(location_vector_A, CHECKPOINT, velocity)
-            checkpoint = self.calcVector(CHECKPOINT, location_vector_B, velocity)
+            path_length = self.__calcVector(location_vector_A, CHECKPOINT, velocity)
+            checkpoint = self.__calcVector(CHECKPOINT, location_vector_B, velocity)
             TIME = (path_length) + TIME + DROPOFF + checkpoint
 
             # calculate the path/time to return to the next pickup point
@@ -153,16 +146,16 @@ class Manufacturing():
             #print(self.data.loc[ next_index , 'Code'])
             #print(self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )])
 
-            TIME = (self.calcVector(next_pickup_vector, location_vector_B, velocity) ) + TIME + DROPOFF + PICKUP
+            TIME = (self.__calcVector(next_pickup_vector, location_vector_B, velocity) ) + TIME + DROPOFF + PICKUP
 
         elif row.Task == 'Fiducial':
-            path_length = self.calcVector((0, 0), location_vector_B, velocity)
+            path_length = self.__calcVector((0, 0), location_vector_B, velocity)
             TIME = (path_length) + TIME
 
         else:
           # all components get treated with single pick
-          path_length = self.calcVector(location_vector_A, CHECKPOINT, velocity)
-          checkpoint = self.calcVector(CHECKPOINT, location_vector_B, velocity)
+          path_length = self.__calcVector(location_vector_A, CHECKPOINT, velocity)
+          checkpoint = self.__calcVector(CHECKPOINT, location_vector_B, velocity)
           TIME = (path_length) + TIME + DROPOFF + checkpoint
           next_index = index + 1
           while next_index not in self.data.index:
@@ -173,7 +166,7 @@ class Manufacturing():
             next_index = next_index +1
           nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )]
           next_pickup_vector = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
-          TIME = (self.calcVector(location_vector_B, next_pickup_vector, velocity)) + TIME + DROPOFF + PICKUP
+          TIME = (self.__calcVector(location_vector_B, next_pickup_vector, velocity)) + TIME + DROPOFF + PICKUP
         
         # saving coordinates for visual plotting
         plotting_x.append(location_vector_B[0])
@@ -200,5 +193,10 @@ class Manufacturing():
 
 
 if __name__ == '__main__':
+  from simulation.cartsetup import CartSetup
+  from simulation.dataloader import DataLoader
   path = Path('/data/26AAWAB')
   dataloader = DataLoader(path)
+  machine = Machine('M20', 19000, 4)
+  manufacturing = Manufacturing(dataloader(), machine)
+  manufacturing(multiPickOption=True)
