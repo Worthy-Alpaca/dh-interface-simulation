b698f486273b26a24d09658abf2a2711f50f0d93
Worthy Alpaca <stephanxxschumacher>
2022-05-04 16:33:15 +0200

    added support for addtional information on canvas

--- src/modules/gui/controller/controller.py
+++ src/modules/gui/controller/controller.py
@@ -1,6 +1,7 @@
 
 
 import os
+import random
 import sys
 
 from gui.parent.canvas import Canvas
@@ -10,8 +11,22 @@ class Controller(Canvas):
     def __init__(self, frame) -> None:
         super().__init__(frame)
 
-    def __call__(self, data_x, data_y) -> any:
+    def __call__(self, data_x, data_y, time, numParts) -> any:
         self.figure.clear()
-        plot = self.figure.add_subplot(111)
+        plot = self.figure.add_subplot(121)
+        ax = self.figure.add_subplot(122)
+        ax.axis('off')
+        sumtime = []
+        for i in range(numParts):
+            sumtime.append(time + random.randint(0, 30))
+        
+        textstr = '\n'.join((
+            f'Overall time needed: {round(sum(sumtime), 2)} Seconds',
+            f'Average Time: {round(sum(sumtime) / numParts, 2)} Seconds ', 
+            f'Highest time: {round(max(sumtime), 2)} Seconds',))
+        props = dict(boxstyle='round', alpha=0.5)
+        
+        ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
         plot.scatter(data_x, data_y)
         self.canvas.draw()
