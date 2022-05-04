6efced51e44bb805023b122659704d8325fce2f9
Worthy Alpaca <stephanxxschumacher>
2022-05-04 16:34:10 +0200

    fixed mulitthreading

--- src/modules/simulation/manufacturing.py
+++ src/modules/simulation/manufacturing.py
@@ -1,8 +1,8 @@
-from email.policy import Policy
 import math
 import random
 import sys
 import os
+import itertools
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import deque
@@ -34,6 +34,7 @@ class Manufacturing():
     self.components = data[1]
     self.data = data[0]
     self.offsets = data[2]
+    
 
   def __calcVector(self, vectorA: tuple, vectorB: tuple, velocity: float):
     """
@@ -47,170 +48,171 @@ class Manufacturing():
     return path_length / velocity
 
   def calcTime(self, offset_row):
-      def isNan(string):
-        return string != string
-    
-      #print('Offsets', type(self.offsets))
-      TIME = 0
-      plotting_x = []
-      plotting_y = []
-      multiPick = deque()
-      #print('Calculating assembly time')
-      #print(offset_row)
-      for index, row in self.data.iterrows():
-        # check for NaN values and continue if found
-        if isNan(row.Code):
-          print(row.Code)
-          continue
+    def isNan(string):
+      return string != string
+  
+    #print('Offsets', type(self.offsets))
+    TIME = 0
+    self.plotting_x = []
+    self.plotting_y = []
+    multiPick = deque()
+    #print('Calculating assembly time')
+    #print(offset_row)
+    for index, row in self.data.iterrows():
+      # check for NaN values and continue if found
+      if isNan(row.Code):
+        print(row.Code)
+        continue
+      # calculating the path, adding offset for coordinate transformation
+      lookupTable = self.components[self.components['index'].str.match(row.Code)]
+      #print(lookupTable)
+      location_vector_A = (lookupTable.Pickup_X.max(), lookupTable.Pickup_Y.max())
+      location_vector_B = ((row.X + OFFSET_X + offset_row[0]), (row.Y + OFFSET_Y + offset_row[1]))
+      velocity = self.machine.velocity * ( lookupTable.mean_acceleration.max() / 1000)
+      
+      if self.multiPickOption == True:
+        # picking components with multiple heads at once
+        # path changes from "Pickup -> Component" to "Pickup1 -> Pickup2 -> Pickup3 -> Component3 -> Component2 -> Component1" 
+        if row.Task == 'Multiple Pickup':
+          # append component vector to queue
+          multiPick.append(location_vector_B)
 
-        # calculating the path, adding offset for coordinate transformation
-        lookupTable = self.components[self.components['index'].str.match(row.Code)]
-        #print(lookupTable)
-        location_vector_A = (lookupTable.Pickup_X.max(), lookupTable.Pickup_Y.max())
-        location_vector_B = ((row.X + OFFSET_X + offset_row[0]), (row.Y + OFFSET_Y + offset_row[1]))
-        velocity = self.machine.velocity * ( lookupTable.mean_acceleration.max() / 1000)
-        
-        if self.multiPickOption == True:
-          # picking components with multiple heads at once
-          # path changes from "Pickup -> Component" to "Pickup1 -> Pickup2 -> Pickup3 -> Component3 -> Component2 -> Component1" 
-          if row.Task == 'Multiple Pickup':
-            # append component vector to queue
-            multiPick.append(location_vector_B)
+          # calculate path/time to next pickup
+          next_index = index +1
+          while next_index not in self.data.index:
+            if next_index >= self.data.index.max():
+              next_index = next_index - 1
+              break
+            next_index = next_index +1
+          nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code'])]
+          next_pickup = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
+          TIME = self.__calcVector(location_vector_A, next_pickup, velocity) + TIME + PICKUP
 
-            # calculate path/time to next pickup
-            next_index = index +1
-            while next_index not in self.data.index:
-              if next_index >= self.data.index.max():
-                next_index = next_index - 1
-                break
-              next_index = next_index +1
-            nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code'])]
-            next_pickup = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
-            TIME = self.__calcVector(location_vector_A, next_pickup, velocity) + TIME + PICKUP
+        elif row.Task == 'End Multiple Pickup':
+          # calculate the path to the current component
+          loc_vector_A = (lookupTable.Pickup_X.max(), lookupTable.Pickup_Y.max())
+          loc_vector_B = ((row.X + OFFSET_X + offset_row[0]), (row.Y + OFFSET_Y + offset_row[1]))
+          checkpoint = self.__calcVector(loc_vector_A, CHECKPOINT, velocity)
+          path = self.__calcVector(CHECKPOINT, loc_vector_B, velocity)
+          TIME = path + TIME + DROPOFF + checkpoint
 
-          elif row.Task == 'End Multiple Pickup':
-            # calculate the path to the current component
-            loc_vector_A = (lookupTable.Pickup_X.max(), lookupTable.Pickup_Y.max())
-            loc_vector_B = ((row.X + OFFSET_X + offset_row[0]), (row.Y + OFFSET_Y + offset_row[1]))
-            checkpoint = self.__calcVector(loc_vector_A, CHECKPOINT, velocity)
-            path = self.__calcVector(CHECKPOINT, loc_vector_B, velocity)
-            TIME = path + TIME + DROPOFF + checkpoint
+          # set the current component vector as the current postition
+          current_pos = location_vector_B
 
-            # set the current component vector as the current postition
-            current_pos = location_vector_B
+          # rotate the queue
+          multiPick.rotate()
 
-            # rotate the queue
-            multiPick.rotate()
+          # loop over queue
+          for i in multiPick:
+            # calculate path and time between components
+            multiPath = self.__calcVector(current_pos, i, velocity)
+            TIME = (multiPath) + TIME + DROPOFF
+            current_pos = i
+          multiPick.clear()
 
-            # loop over queue
-            for i in multiPick:
-              # calculate path and time between components
-              multiPath = self.__calcVector(current_pos, i, velocity)
-              TIME = (multiPath) + TIME + DROPOFF
-              current_pos = i
-            multiPick.clear()
-
-            # calculate the path/time to return to the next pickup point
-            next_index = index +1
-            while next_index not in self.data.index:
-              if next_index >= self.data.index.max():
-                next_index = next_index - 1
-                break
-              next_index = next_index +1
-            nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )]
-            next_pickup_vector = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
-            TIME = (self.__calcVector(next_pickup_vector, current_pos, velocity)) + TIME + DROPOFF
-
-          elif row.Task == 'Fiducial':
-            path_length = self.__calcVector((0, 0), location_vector_B, velocity)
-            TIME = (path_length) + TIME 
-
-          else:
-            #print('==  single pick, multipick opt  ==')
-            # calculate the path/time for a single pickup
-            path_length = self.__calcVector(location_vector_A, CHECKPOINT, velocity)
-            checkpoint = self.__calcVector(CHECKPOINT, location_vector_B, velocity)
-            TIME = (path_length) + TIME + DROPOFF + checkpoint
-
-            # calculate the path/time to return to the next pickup point
-            next_index = index +1
-            while next_index not in self.data.index:
-              if next_index > self.data.index.max():
-                next_index = next_index - 1
-                break
-              next_index = next_index +1
-            nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )]
-            next_pickup_vector = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
-            #print('it breaks here')
-            #print(self.data.loc[ next_index , 'Code'])
-            #print(self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )])
-
-            TIME = (self.__calcVector(next_pickup_vector, location_vector_B, velocity) ) + TIME + DROPOFF + PICKUP
+          # calculate the path/time to return to the next pickup point
+          next_index = index +1
+          while next_index not in self.data.index:
+            if next_index >= self.data.index.max():
+              next_index = next_index - 1
+              break
+            next_index = next_index +1
+          nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )]
+          next_pickup_vector = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
+          TIME = (self.__calcVector(next_pickup_vector, current_pos, velocity)) + TIME + DROPOFF
 
         elif row.Task == 'Fiducial':
-            path_length = self.__calcVector((0, 0), location_vector_B, velocity)
-            TIME = (path_length) + TIME
+          path_length = self.__calcVector((0, 0), location_vector_B, velocity)
+          TIME = (path_length) + TIME 
 
         else:
-          # all components get treated with single pick
+          #print('==  single pick, multipick opt  ==')
+          # calculate the path/time for a single pickup
           path_length = self.__calcVector(location_vector_A, CHECKPOINT, velocity)
           checkpoint = self.__calcVector(CHECKPOINT, location_vector_B, velocity)
           TIME = (path_length) + TIME + DROPOFF + checkpoint
-          next_index = index + 1
+
+          # calculate the path/time to return to the next pickup point
+          next_index = index +1
           while next_index not in self.data.index:
-            #next_index = next_index +1
             if next_index > self.data.index.max():
               next_index = next_index - 1
               break
             next_index = next_index +1
           nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )]
           next_pickup_vector = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
-          TIME = (self.__calcVector(location_vector_B, next_pickup_vector, velocity)) + TIME + DROPOFF + PICKUP
-        
-        # saving coordinates for visual plotting
-        plotting_x.append(location_vector_B[0])
-        plotting_y.append(location_vector_B[1])
-        return {
-          'time': TIME + random.randint(0, 30),
-          'plot_x': plotting_x,
-          'plot_y': plotting_y
-        }
+          #print('it breaks here')
+          #print(self.data.loc[ next_index , 'Code'])
+          #print(self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )])
 
-  def __call__(self, multiPickOption: bool = True, plotPCB: bool = False, multithread = False, *args: any, **kwds: any) -> (int | dict):
+          TIME = (self.__calcVector(next_pickup_vector, location_vector_B, velocity) ) + TIME + DROPOFF + PICKUP
+
+      elif row.Task == 'Fiducial':
+          path_length = self.__calcVector((0, 0), location_vector_B, velocity)
+          TIME = (path_length) + TIME
+
+      else:
+        # all components get treated with single pick
+        path_length = self.__calcVector(location_vector_A, CHECKPOINT, velocity)
+        checkpoint = self.__calcVector(CHECKPOINT, location_vector_B, velocity)
+        TIME = (path_length) + TIME + DROPOFF + checkpoint
+        next_index = index + 1
+        while next_index not in self.data.index:
+          #next_index = next_index +1
+          if next_index > self.data.index.max():
+            next_index = next_index - 1
+            break
+          next_index = next_index +1
+        nextLookUpTable = self.components[self.components['index'].str.match( self.data.loc[ next_index , 'Code']  )]
+        next_pickup_vector = (nextLookUpTable.Pickup_X.max(), nextLookUpTable.Pickup_Y.max())
+        TIME = (self.__calcVector(location_vector_B, next_pickup_vector, velocity)) + TIME + DROPOFF + PICKUP
+      
+      # saving coordinates for visual plotting
+      self.plotting_x.append(location_vector_B[0])
+      self.plotting_y.append(location_vector_B[1])
+    return {
+      'time': TIME,
+      'plot_x': self.plotting_x,
+      'plot_y': self.plotting_y
+    }
+
+  def __call__(self, multiPickOption: bool = True, plotPCB: bool = False, multithread: bool = False, *args: any, **kwds: any) -> (int | dict):
     """ Start the assembly simulation """
     self.multiPickOption = multiPickOption
     # for offset_index, offset_row in self.offsets.iterrows():
     if multithread == True:
       pool = Pool(len(self.offsets))
-      test = pool.map(self.calcTime, self.offsets)
+      pool_data = pool.map(self.calcTime, self.offsets)
+      pool.close()
+      pool.join()
       time = 0
       plotX = []
       plotY = []
-      for i in test:
+      for i in pool_data:
         time = time + i['time']
         plotX.append(i['plot_x'])
         plotY.append(i['plot_y'])
       if plotPCB == True:
         return {
-          'time': time + random.randint(0, 30),
-          'plot_x': plotX,
-          'plot_y': plotY
+          'time': time,
+          'plot_x': list(itertools.chain.from_iterable(plotX)),
+          'plot_y': list(itertools.chain.from_iterable(plotY))
         }
-      return time #+ random.randint(0,30)
+      return time
     else:
       time = 0
       plotX = []
       plotY = []
       for i in self.offsets:
-        test = self.calcTime(i)
-        time = time + test['time']
-        plotX.append(test['plot_x'])
-        plotY.append(test['plot_y'])
+        iter_data = self.calcTime(i)
+        time = time + iter_data['time']
+        plotX.append(iter_data['plot_x'])
+        plotY.append(iter_data['plot_y'])
       if plotPCB == True:
         return {
-          'time': time + random.randint(0, 30),
-          'plot_x': plotX,
-          'plot_y': plotY
+          'time': time,
+          'plot_x': list(itertools.chain.from_iterable(plotX)),
+          'plot_y': list(itertools.chain.from_iterable(plotY))
         }
       return time
   
@@ -227,8 +229,9 @@ class Manufacturing():
 if __name__ == '__main__':
   from simulation.cartsetup import CartSetup
   from simulation.dataloader import DataLoader
-  path =  Path(os.getcwd() + os.path.normpath('/data/26AAWAB'))
+  path =  Path(os.getcwd() + os.path.normpath('/data/3160194'))
   dataloader = DataLoader(path)
   machine = Machine('M20', 19000, 4)
   manufacturing = Manufacturing(dataloader(), machine)
-  manufacturing(multiPickOption=True, multithread=True)
+  manuData = manufacturing(multiPickOption=True, multithread=False, plotPCB=True)
+  print(manuData['plot_x'])
