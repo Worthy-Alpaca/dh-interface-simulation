77bdd92ae268fe8712609c4d1ac0bed11d4b87a0
Worthy Alpaca <stephanxxschumacher>
2022-05-05 14:32:23 +0200

    added support for multiple machines

--- src/modules/gui/controller/controller.py
+++ src/modules/gui/controller/controller.py
@@ -11,19 +11,24 @@ class Controller(Canvas):
     def __init__(self, frame) -> None:
         super().__init__(frame)
 
-    def __call__(self, data_x, data_y, time, numParts, randomInterupt: tuple = (0, 0)) -> any:
+    def __call__(self, data_x, data_y, time: dict, numParts: int, randomInterupt: tuple = (0, 0)) -> any:
         self.figure.clear()
         plot = self.figure.add_subplot(121)
         ax = self.figure.add_subplot(122)
+        runtime = sum(list(time.values()))
         ax.axis('off')
         sumtime = []
         for i in range(numParts):
-            sumtime.append(time + random.randint(randomInterupt[0], randomInterupt[1]))
+            sumtime.append(runtime + random.randint(randomInterupt[0], randomInterupt[1]))
         
         textstr = '\n'.join((
             f'Overall time needed: {round(sum(sumtime), 2)} Seconds',
             f'Average Time: {round(sum(sumtime) / numParts, 2)} Seconds ', 
             f'Highest time: {round(max(sumtime), 2)} Seconds',))
+        substr = ''
+        for key in time:
+            substr = substr + f'\n{key} Ideal: {round(time[key], 2)} Seconds'
+        textstr = textstr + substr
         props = dict(boxstyle='round', alpha=0.5)
         
         ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
