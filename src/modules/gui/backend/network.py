093ffa1ef0387a3bd8abd1017b037295b0676192
Worthy Alpaca <stephanxxschumacher>
2022-05-20 13:12:31 +0200

    exported API calls to provide easier usage

---
+++ src/modules/gui/backend/network.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+
+class NetworkRequests:
+    def __init__(self, networkAddress: str, basePath: str) -> None:
+        self.networkAddress = networkAddress + basePath
+
+    def get(self, endpoint: str, params: dict = {}) -> (tuple[bool, Exception] | dict):
+        queryParams = "?"
+        for key in params:
+            queryParams = queryParams + f"{key}={params[key]}&"
+
+        try:
+            request = requests.get(self.networkAddress + endpoint + queryParams)
+        except Exception as e:
+            return (False, e)
+
+        return request.json()
+
+    def put(self, endpoint: str, params: dict, data: dict):
+        queryParams = "?"
+        for key in params:
+            queryParams = queryParams + f"{key}={params[key]}&"
+
+        try:
+            request = requests.put(
+                self.networkAddress + endpoint + queryParams, data=json.dump(data)
+            )
+        except Exception as e:
+            return (False, e)
+
+        return request
